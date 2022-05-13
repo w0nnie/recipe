ec690a1110f9d28907720f6b1b3dfcd6ec39203b
jinwon <kjw55523@gmail.com>
2022-05-13 18:01:46 +0900

    Initial commit

---
+++ recipe/src/test/java/toy/recipe/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
