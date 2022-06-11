82bbb47a7242fd62fb99a85a2a0fbb91a2d76cfd
jinwon <kjw55523@gmail.com>
2022-06-11 22:44:03 +0900

    fix: ingredient 화면단 구성(#013)
        jQuery checked를 사용하여 check됐을때 value값만 console에 찍어준다

--- recipe/src/main/resources/static/assets/js/app.js
+++ recipe/src/main/resources/static/assets/js/app.js
@@ -2,32 +2,40 @@ const kcal = document.getElementById("jsRange");
 const way = document.getElementById("way");
 const type = document.getElementById("type");
 
-function getProduct(){
-    var path = "pages/main";
-    var resultFrag = 'getProductAxDiv';
-    const name = document.getElementById("name").value;
-    getProductAx(path, resultFrag, name);
+function getProduct() {
+  var path = "pages/main";
+  var resultFrag = "getProductAxDiv";
+  const name = document.getElementById("name").value;
+  getProductAx(path, resultFrag, name);
 }
 
-function rangeChange(){
-     var path = "pages/main";
-     var resultFrag = 'getProductAxDiv';
-     const kvalue =  kcal.value;
-     const kvalueInt = parseFloat(kvalue);
-     const wvalue = way.value;
-     const tvalue = type.value;
-    getTypeWayKcalAx(path, resultFrag,wvalue,tvalue,kvalueInt);
+function rangeChange() {
+  var path = "pages/main";
+  var resultFrag = "getProductAxDiv";
+  const kvalue = kcal.value;
+  const kvalueInt = parseFloat(kvalue);
+  const wvalue = way.value;
+  const tvalue = type.value;
+  getTypeWayKcalAx(path, resultFrag, wvalue, tvalue, kvalueInt);
 }
 
-
-function getCheckboxValue(event){
-    let result = '';
-    console.log(event.target.value);
-    // if(event.target.checked)  {
-    //     result = event.target.value;
-    //     console.log(result)
-    //  }else {
-    //     result = '';
-    //     console.log(result)
-    //  }
+function getCheckboxValue(event) {
+  console.log(event.value);
+  // if(event.target.checked)  {
+  //     result = event.target.value;   
+  //     console.log(result)
+  //  }else {
+  //     result = '';
+  //     console.log(result)
+  //  }
 }
+
+$('#check').click(function(){
+    var checked = $('#check').is(':checked');
+
+    if(checked){
+        console.log($('#check').val());
+    }else{
+        console.log(" ");
+    }
+})
