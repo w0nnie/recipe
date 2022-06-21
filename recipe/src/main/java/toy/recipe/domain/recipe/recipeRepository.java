60ce3e8092f8321409476ae2b96e132ff6978fcc
jinwon <kjw55523@gmail.com>
2022-06-21 17:34:22 +0900

    Chore: recipe project jpa (#016)
         기존의 native query 주석으로 추가

--- recipe/src/main/java/toy/recipe/domain/recipe/recipeRepository.java
+++ recipe/src/main/java/toy/recipe/domain/recipe/recipeRepository.java
@@ -3,6 +3,7 @@ package toy.recipe.domain.recipe;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import toy.recipe.domain.member.member;
@@ -17,15 +18,61 @@ public interface recipeRepository extends JpaRepository<recipe, Long> {
 
     recipe findByRcpName(String name);
 
+//    @Query(nativeQuery = true,
+//            value = "SELECT *\n" +
+//                    "FROM recipe\n" +
+//                    "WHERE RCP_NM = ?1\n" +
+//                    "ORDER BY RCP_SEQ")
+//    recipe findByName(String name);
+
     List<recipe> findByRcpPatAndInfoEngLessThanEqual(String type, Float kcal, Pageable pageable);
+
+//    @Query(nativeQuery = true,
+//            value = "SELECT *\n" +
+//                    "FROM recipe\n"+
+//                    "WHERE RCP_PAT2 = ?1\n" +
+//                    "AND INFO_ENG <= ?2\n" +
+//                    "ORDER BY RCP_SEQ")
+//    List<recipe> findByTypeKcal(String type, Float kcal, Pageable pageable);
 
     List<recipe> findByRcpWayAndInfoEngLessThanEqual(String way, Float kcal, Pageable pageable);
 
+//    @Query(nativeQuery = true,
+//            value = "SELECT *\n" +
+//                    "FROM recipe\n"+
+//                    "WHERE RCP_WAY2 = ?1\n" +
+//                    "AND INFO_ENG <= ?2\n" +
+//                    "ORDER BY RCP_SEQ")
+//    List<recipe> findByWayKcal(String way, Float kcal, Pageable pageable);
+
     List<recipe> findByRcpWayAndRcpPatAndInfoEngLessThanEqual(String way, String type, Float kcal, Pageable pageable);
+
+//    @Query(nativeQuery = true,
+//            value = "SELECT *\n" +
+//                    "FROM recipe\n"+
+//                    "WHERE RCP_WAY2 = ?1\n" +
+//                    "AND RCP_PAT2 = ?2\n" +
+//                    "AND INFO_ENG <= ?3\n" +
+//                    "ORDER BY RCP_SEQ")
+//    List<recipe> findByWayTypeKcal(String way, String type, Float kcal, Pageable pageable);
 
     List<recipe> findByInfoEngLessThanEqual(Float kcal, Pageable pageable);
 
+//    @Query(nativeQuery = true,
+//            value = "SELECT *\n" +
+//                    "FROM recipe\n" +
+//                    "WHERE INFO_ENG <= ?1\n" +
+//                    "ORDER BY RCP_SEQ")
+//    List<recipe> findByKcal(Float kcal, Pageable pageable);
+
     List<recipe> findByRcpPartsDtlsContaining(String value, Pageable pageable);
+
+//    @Query(nativeQuery = true,
+//            value = "SELECT *\n" +
+//                    "FROM recipe\n" +
+//                    "WHERE RCP_PARTS_DTLS LIKE %?1%\n" +
+//                    "ORDER BY RCP_SEQ")
+//    List<recipe> findByIngredient(String value, Pageable pageable);
 
     @Query(nativeQuery = true,
             value = "SELECT *\n" +
@@ -43,4 +90,6 @@ public interface recipeRepository extends JpaRepository<recipe, Long> {
                     "AND RCP_PARTS_DTLS LIKE %?3%\n" +
                     "ORDER BY RCP_SEQ")
     List<recipe> findByIngredient3(String value1, String value2, String value3, Pageable pageable);
+
+
 }
