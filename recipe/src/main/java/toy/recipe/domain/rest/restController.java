8188fe27869bf62eceed78ac70567e748514d52f
jinwon <kjw55523@gmail.com>
2022-07-10 22:25:47 +0900

    Chore: RestController(#018)
         restController
         -> recipe 내용 구현

--- recipe/src/main/java/toy/recipe/domain/rest/restController.java
+++ recipe/src/main/java/toy/recipe/domain/rest/restController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import toy.recipe.domain.member.member;
 import toy.recipe.domain.member.memberRepository;
+import toy.recipe.domain.recipe.recipe;
 import toy.recipe.domain.recipe.recipeRepository;
 
 import java.util.List;
@@ -25,7 +26,7 @@ public class restController {
 
 
     @GetMapping("/member")
-    public ResponseEntity<CommonResponse<List<member>>> findAll() {
+    public ResponseEntity<CommonResponse<List<member>>> findAllMember() {
 
         return new ResponseEntity<>(new CommonResponse<>(HttpStatus.OK.value(), memberRepository.findAll()),HttpStatus.OK);
     }
@@ -61,7 +62,7 @@ public class restController {
         return new ResponseEntity<>(new CommonResponse<>(HttpStatus.CREATED.value(), "Success"),HttpStatus.OK);
     }
 
-    @PatchMapping("/member/{id}/{password}") // TODO: 2022/07/04
+    @PatchMapping("/member/{id}/{password}")
     public ResponseEntity<? extends CommonDto> update(@PathVariable String id, @PathVariable int password){
 
         member member = memberRepository.findById(id);
@@ -73,6 +74,114 @@ public class restController {
             member.setPassword(password);
             memberRepository.save(member);
             return new ResponseEntity<>(new CommonResponse<>(HttpStatus.CREATED.value(), "Success"),HttpStatus.CREATED);
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @GetMapping("/recipe") //http://localhost:8080/api/recipe
+    public ResponseEntity<CommonResponse<List<recipe>>> findAllRecipe() {
+
+        return new ResponseEntity<>(new CommonResponse<>(HttpStatus.OK.value(), recipeRepository.findAll()),HttpStatus.OK);
+    }
+
+    @GetMapping("/recipe/{rcpName}") //http://localhost:8080/api/recipe/rcpName
+    public ResponseEntity<? extends CommonDto> findByRcpName(@PathVariable String rcpName){
+        recipe recipe = recipeRepository.findByRcpName(rcpName);
+
+        if(recipe == null){
+            return ResponseEntity.status(HttpStatus.OK).body(new ErrorResponse(HttpStatus.OK.value(), "일치하는 레시피 정보가 없습니다."));
+        } else {
+            return new ResponseEntity<>(new CommonResponse<>(HttpStatus.OK.value(), recipe),HttpStatus.OK);
+        }
+
+    }
+
+    @GetMapping("/recipe/type/{RcpPat}/{InfoEng}") //http://localhost:8080/api/recipe/type/반찬/300
+    public ResponseEntity<? extends CommonDto> findByRcpPatAndInfoEngLessThanEqual(@PathVariable String RcpPat, @PathVariable Float InfoEng){
+        List<recipe> recipeList = recipeRepository.findByRcpPatAndInfoEngLessThanEqual(RcpPat, InfoEng);
+
+        if(recipeList.isEmpty()){
+            return ResponseEntity.status(HttpStatus.OK).body(new ErrorResponse(HttpStatus.OK.value(), "일치하는 레시피 정보가 없습니다."));
+        } else {
+            return new ResponseEntity<>(new CommonResponse<>(HttpStatus.OK.value(), recipeList),HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/recipe/way/{rcpWay}/{infoEng}") //http://localhost:8080/api/recipe/way/끊이기/300
+    public ResponseEntity<? extends CommonDto> findByRcpWayAndInfoEngLessThanEqual(@PathVariable String rcpWay, @PathVariable Float infoEng){
+        List<recipe> recipeList = recipeRepository.findByRcpWayAndInfoEngLessThanEqual(rcpWay, infoEng);
+
+        if(recipeList.isEmpty()){
+            return ResponseEntity.status(HttpStatus.OK).body(new ErrorResponse(HttpStatus.OK.value(), "일치하는 레시피 정보가 없습니다."));
+        } else {
+            return new ResponseEntity<>(new CommonResponse<>(HttpStatus.OK.value(), recipeList),HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/recipe/{rcpWay}/{rcpPat}/{infoEng}") //http://localhost:8080/api/recipe/끊이기/밥/300
+    public ResponseEntity<? extends CommonDto> findByRcpWayAndRcpPatAndInfoEngLessThanEqual(@PathVariable String rcpWay, @PathVariable String rcpPat, @PathVariable Float infoEng){
+        List<recipe> recipeList = recipeRepository.findByRcpWayAndRcpPatAndInfoEngLessThanEqual(rcpWay, rcpPat, infoEng);
+
+        if(recipeList.isEmpty()){
+            return ResponseEntity.status(HttpStatus.OK).body(new ErrorResponse(HttpStatus.OK.value(), "일치하는 레시피 정보가 없습니다."));
+        } else {
+            return new ResponseEntity<>(new CommonResponse<>(HttpStatus.OK.value(), recipeList),HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/recipe/kcal/{infoEng}") //http://localhost:8080/api/recipe/kcal/300
+    public ResponseEntity<? extends CommonDto> findByInfoEngLessThanEqual(@PathVariable Float infoEng){
+        List<recipe> recipeList = recipeRepository.findByInfoEngLessThanEqual(infoEng);
+
+        if(recipeList.isEmpty()){
+            return ResponseEntity.status(HttpStatus.OK).body(new ErrorResponse(HttpStatus.OK.value(), "일치하는 레시피 정보가 없습니다."));
+        } else {
+            return new ResponseEntity<>(new CommonResponse<>(HttpStatus.OK.value(), recipeList),HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/recipe/ingredient/{rcpPartsDtls}") //http://localhost:8080/api/recipe/ingredient/돼지고기
+    public ResponseEntity<? extends CommonDto> findByRcpPartsDtlsContaining(@PathVariable String rcpPartsDtls){
+        List<recipe> recipeList = recipeRepository.findByRcpPartsDtlsContaining(rcpPartsDtls);
+
+        if(recipeList.isEmpty()){
+            return ResponseEntity.status(HttpStatus.OK).body(new ErrorResponse(HttpStatus.OK.value(), "일치하는 레시피 정보가 없습니다."));
+        } else {
+            return new ResponseEntity<>(new CommonResponse<>(HttpStatus.OK.value(), recipeList),HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/recipe/ingredient/{rcpPartsDtls}/{rcpPartsDtls2}") //http://localhost:8080/api/recipe/ingredient/돼지고기/배추
+    public ResponseEntity<? extends CommonDto> findByRcpPartsDtlsContaining(@PathVariable String rcpPartsDtls, @PathVariable String rcpPartsDtls2){
+        List<recipe> recipeList = recipeRepository.findByRcpPartsDtlsContainingAndRcpPartsDtlsContaining(rcpPartsDtls, rcpPartsDtls2);
+
+        if(recipeList.isEmpty()){
+            return ResponseEntity.status(HttpStatus.OK).body(new ErrorResponse(HttpStatus.OK.value(), "일치하는 레시피 정보가 없습니다."));
+        } else {
+            return new ResponseEntity<>(new CommonResponse<>(HttpStatus.OK.value(), recipeList),HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/recipe/ingredient/{rcpPartsDtls}/{rcpPartsDtls2}//{rcpPartsDtls3}") //http://localhost:8080/api/recipe/ingredient/돼지고기/배추/밥
+    public ResponseEntity<? extends CommonDto> findByRcpPartsDtlsContainingAndRcpPartsDtlsContainingAndRcpPartsDtlsContaining(@PathVariable String rcpPartsDtls, @PathVariable String rcpPartsDtls2, @PathVariable String rcpPartsDtls3){
+        List<recipe> recipeList = recipeRepository.findByRcpPartsDtlsContainingAndRcpPartsDtlsContainingAndRcpPartsDtlsContaining(rcpPartsDtls, rcpPartsDtls2, rcpPartsDtls3);
+
+        if(recipeList.isEmpty()){
+            return ResponseEntity.status(HttpStatus.OK).body(new ErrorResponse(HttpStatus.OK.value(), "일치하는 레시피 정보가 없습니다."));
+        } else {
+            return new ResponseEntity<>(new CommonResponse<>(HttpStatus.OK.value(), recipeList),HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/recipe/ingredient/{rcpPartsDtls}/{rcpPartsDtls2}/{rcpPartsDtls3}/{rcpPartsDtls4}") //http://localhost:8080/api/recipe/ingredient/돼지고기/배추/밥/고추
+    public ResponseEntity<? extends CommonDto> findByRcpPartsDtlsContainingAndRcpPartsDtlsContainingAndRcpPartsDtlsContainingAndRcpPartsDtlsContaining(@PathVariable String rcpPartsDtls, @PathVariable String rcpPartsDtls2, @PathVariable String rcpPartsDtls3, @PathVariable String rcpPartsDtls4){
+        List<recipe> recipeList = recipeRepository.findByRcpPartsDtlsContainingAndRcpPartsDtlsContainingAndRcpPartsDtlsContainingAndRcpPartsDtlsContaining(rcpPartsDtls, rcpPartsDtls2, rcpPartsDtls3, rcpPartsDtls4);
+
+        if(recipeList.isEmpty()){
+            return ResponseEntity.status(HttpStatus.OK).body(new ErrorResponse(HttpStatus.OK.value(), "일치하는 레시피 정보가 없습니다."));
+        } else {
+            return new ResponseEntity<>(new CommonResponse<>(HttpStatus.OK.value(), recipeList),HttpStatus.OK);
         }
     }
 }
